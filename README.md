17a0431a5c6f48d520957691d656c79a7e5b66dc
John Akhilomen <jonbasanti@gmail.com>
2021-08-15 23:30:06 -0500

    Updated Readme

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # flask_api
 
+## Install dependencies
+```
+ pip install -r requirements.txt     
+```
+
 ## Build with docker
 ```python
 docker-compose build
@@ -8,4 +13,9 @@ docker-compose build
 ## Run with docker
 ```python
 docker-compose up
+```
+## Run app locally
+while on the web directory run
+```
+python app.py
 ```
--- web/resources/User.py
+++ web/resources/User.py
@@ -1,6 +1,41 @@
 from flask_restful import Api, Resource
+from flask import jsonify, request
 
+# tenderResults,
+# toughResults,
+# intuitionResult,
+# sensingResult,
+# introvertResult,
+# extrovertResult,
+
+def check_posted_data():
+    props = ('email', 'tenderResults', 'toughResults', 'intuitionResult', 'sensingResult', 'introvertResult', 'extrovertResult')
+    dict = request.get_json()
+    if props not in dict:
+        return 'ERROR', 404
+    return jsonify(dict), 200
+    
 class User(Resource):
-    def post(self):
-        
+    def post(self, userDapper):
+        dict = request.get_json()
+        status_code = check_posted_data(dict)
+        if status_code != 200:
+            return {
+                'result': "Invalid request body",
+                "status_code": status_code
+            }, status_code
+
+        user = userDapper.findOne({'email' : dict.email});
+        if(user):
+            return {
+                'result': "User already exist",
+                "status_code": status_code
+            }, status_code
+        res = userDapper.insert(user) 
+        # Usernumber.update({}, {
+        #     "$set": {
+        #         "num_of_users": prev_num
+        #         }
+        #     })
+        return jsonify(res), 200
 
