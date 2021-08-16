204070d14b082349ddebb75c6e6f253474008326
John Akhilomen <jonbasanti@gmail.com>
2021-08-16 02:21:56 -0500

    msg

--- web/resources/User.py
+++ web/resources/User.py
@@ -1,6 +1,8 @@
 from flask_restful import Api, Resource
 from flask import jsonify, request
 from dappers.Dappers import UserDapper
+import json
+from bson import json_util
 
 # tenderResults,
 # toughResults,
@@ -16,8 +18,27 @@ def check_posted_data(dict):
     if 'email' not in dict:
         return 422
     return 200
-    
+
+def build_response(data, status_code):
+    response = {
+            'data': data,
+            "status_code": status_code
+        }, status_code
+    return jsonify(response)
+
 class User(Resource):
+
+    '''
+    Let's find all users. Find() will return a pymongo.cursor.Cursor 
+    We'll get the actual objects by iterating over the cursor to get its results:
+    '''
+    def get(self):
+        # print([doc for doc in UserDapper.user_collection().find()])
+        users = [doc for doc in UserDapper.user_collection().find()]
+        # users = UserDapper.user_collection().find()
+        data = [json.dumps(item, default=json_util.default) for item in users]
+        return jsonify(data)
+
     def post(self):
         dict = request.get_json()
         status_code = check_posted_data(dict)
